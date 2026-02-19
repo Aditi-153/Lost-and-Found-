d711157614af6747654b095ee5ef2c5e5410c397
Aditi-153 <pachpatiladiti@gmail.com>
2026-02-19 23:46:02 +0530

    changes in user schema and listing route

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 export const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        trim : true
+    },
 
     email : {
         type : String,
