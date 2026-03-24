38764d558c3bf24fd49462aa50008d20576f6735
Aditi-153 <pachpatiladiti@gmail.com>
2026-03-25 00:46:13 +0530

    created login and register page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import axios from "axios";
-import { useNavigate, Link } from "react-router-dom";
 
-const login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-}
+import {  Link } from "react-router-dom";
+
+
 
 const Login = () => {
   return (
